3f192a62cc714cd54e2c48895e7bf82dc042f372
Aredli <xtarak@gmail.com>
2022-06-20 17:40:19 +0200

    Update CalendarView.vue

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -2,7 +2,7 @@
   <SectionTitle color="#00b545" title="Événements et Compétitions à venir" />
   <iframe
     height="75%"
-    src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&showTabs=0&showCalendars=0&src=MjZqZ25mbGJ0aWV1NTB2M3F1NjhqZ2kwbThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F09300"
+    src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&showDate=1&showNav=1&showPrint=1&showTabs=0&showCalendars=0&src=MjZqZ25mbGJ0aWV1NTB2M3F1NjhqZ2kwbThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F09300"
     style="border-width: 0"
     width="100%"
   ></iframe>
